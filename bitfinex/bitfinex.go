c053f374e3446f1babe81ee055b7dfb883838ad1
Willem Elbers <willem@clarin.eu>
2015-07-20 16:59:51 +0200

    Changed order of returned result so that the newest record is last in the result list

--- bitfinex/bitfinex.go
+++ bitfinex/bitfinex.go
@@ -4,8 +4,8 @@ import (
 	"github.com/cryptobase/scraper/model"
 	"github.com/cryptobase/scraper/restclient"
 	"encoding/json"
-	//"fmt"
 	"strconv"
+	"fmt"
 )
 
 type BitfinexTrade struct {
@@ -17,26 +17,25 @@ type BitfinexTrade struct {
 	Type		string
 }
 
-//type BitfinexTrades struct {
-//	Trades []BitfinexTrade
-//}
-
 func Scrape(last_timestamp uint32) ([]model.Trade, error) {
-	content, err := restclient.GetContent("https://api.bitfinex.com/v1/trades/btcusd")
+	url := fmt.Sprintf("https://api.bitfinex.com/v1/trades/btcusd?timestamp=%d", last_timestamp+1)
+	content, err := restclient.GetContent(url)
 	if err != nil {
 		return nil, err
 	}
 
-	//var trades BitfinexTrades
-	//err = json.Unmarshal(content, &trades.Trades)
 	bitfinex_trades := make([]BitfinexTrade,0)
 	err = json.Unmarshal(content, &bitfinex_trades	)
 	if err != nil {
 		return nil, err
 	}
 
+	//Bitfinex trade response has latest trade first, reverse
 	trades := []model.Trade{}
-	for _, trade := range bitfinex_trades {
+	last := len(bitfinex_trades)-1
+	for i, _ := range bitfinex_trades {
+		trade := bitfinex_trades[last-i]
+
 		var t model.Trade
 		t.Timestamp = uint32(trade.Timestamp)
 		t.TradeId = trade.Tid
